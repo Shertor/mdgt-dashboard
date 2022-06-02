53f0acd0ae7fc8cfc954e49e49c4cc9dddab87a4
Shertor <shertors@gmail.com>
2022-06-02 14:32:39 +0300

    Update DisplayCard.jsx

--- src/Components/DisplayCard/DisplayCard.jsx
+++ src/Components/DisplayCard/DisplayCard.jsx
@@ -7,11 +7,11 @@ function DisplayCard({ title, prise, date, chartLoaded }) {
 	return (
 		<>
 			<div className="current-prise card-item">
-				<div className="current-prise__title">{title}</div>
+				{title ? <div className="current-prise__title">{title}</div> : null}
 				<div className="current-prise__prise">
 					{chartLoaded ? prise + '%' : null}
 				</div>
-				<div className="current-prise__date">{date}</div>
+				{date ? <div className="current-prise__date">{date}</div> : null}
 			</div>
 		</>
 	)
