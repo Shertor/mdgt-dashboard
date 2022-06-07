a2a078f09571635a9d18a27fb4a05a034030ce7e
Shertor <shertors@gmail.com>
2022-06-07 17:57:06 +0300

    Кнопка закрыть карточку

--- src/Components/DisplayCard/DisplayCard.jsx
+++ src/Components/DisplayCard/DisplayCard.jsx
@@ -4,7 +4,15 @@ import PropTypes from 'prop-types'
 import './DisplayCard.css'
 import { useState } from 'react'
 
-function DisplayCard({ title, prize, date, chartLoaded, type, unit }) {
+function DisplayCard({
+	title,
+	prize,
+	date,
+	chartLoaded,
+	type,
+	unit,
+	closeBtn,
+}) {
 	const [visible, setVisible] = useState(true)
 
 	function onCloseBtn() {
@@ -41,11 +49,13 @@ function DisplayCard({ title, prize, date, chartLoaded, type, unit }) {
 						) : null}
 					</div>
 					<div className="current-prize__date">{date}</div>
-					{/* <button onClick={onCloseBtn} className="current-prize__close-btn">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-							<path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
-						</svg>
-					</button> */}
+					{closeBtn ? (
+						<button onClick={onCloseBtn} className="current-prize__close-btn">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+								<path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+							</svg>
+						</button>
+					) : null}
 				</div>
 			) : null}
 		</>
@@ -59,6 +69,7 @@ DisplayCard.propTypes = {
 	chartLoaded: PropTypes.bool.isRequired,
 	type: PropTypes.string,
 	unit: PropTypes.string,
+	closeBtn: PropTypes.bool,
 }
 
 export default DisplayCard
--- src/Components/Payments/Payments.jsx
+++ src/Components/Payments/Payments.jsx
@@ -106,6 +106,7 @@ export default function Payments({ toSummary }) {
 											}
 											chartLoaded={paymentsLoaded}
 											key={element}
+											closeBtn={true}
 										/>
 									))}
 								</div>
